7546dda26cca0c10908330878ddfaf1b8d43cf6c
Anna W <anski.anna@gmail.com>
2019-07-09 13:58:55 +0100

    delete working

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ server.get('/api/posts', (req, res) => {
 })
 
 server.get('/api/posts/:id', (req, res) => {
-    const userId = req.params.id;
-    db.findById(userId)
+    const postId = req.params.id;
+    db.findById(postId)
         .then(data => {
             if(data){
                 res.status(200).json(data);
@@ -44,6 +44,20 @@ server.post('/api/posts', (req, res) => {
     })
 })
 
+server.delete('/api/posts/:id', (req, res) => {
+    const postId = req.params.id;
+    db.remove(postId)
+        .then(data => {
+            if(data){
+                res.status(200).json(data)
+            }else{
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post could not be removed" })
+        })
+})
 
 server.listen(4001, () => {
     console.log('\n*** Server Running on http://localhost:4001 ***\n');
